66187d91b3324954a3ccfe9decb6753917ae7db7
Klimov Yaroslav <klimov.iav@phystech.edu>
2023-03-23 11:00:20 +0300

    tmp

--- send.php
+++ send.php
@@ -5,5 +5,8 @@ $phone = "Телефон: ".$_POST['phone'].' <br />';
 $problem = "Сообщение: "$_POST['problem'].' <br />';
 $AllInOne =  $fio.$phone.$problem; 
 $to = 'yar.klimoff@yandex.ru'; 
-mail($to, 'Свяжитесь с нами', $AllInOne, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email"); 
+$send = mail($to, 'Свяжитесь с нами', $AllInOne, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email"); 
+if ($send == 'true') {echo "Сообщение отправлено";}
+// Если письмо не ушло — выводим сообщение об ошибке
+else {echo "Ой, что-то пошло не так";}
 ?>
